f21c9e3351cc1321e8579daa43a6a83254cecc99
enandan <jl830630@gmail.com>
2020-05-28 19:51:47 -0700

    added deployed url

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 # Overview
 
 In this assignment, you'll create a burger logger with MySQL, Node, Express, Handlebars and a homemade ORM (yum!). Be sure to follow the MVC design pattern; use Node and MySQL to query and route data in your app, and Handlebars to generate your HTML.
+
+# Depolyed URL
+https://rocky-reef-43190.herokuapp.com/
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO `burgers_db`.`burgers` (`burger_name`, `devoured`) VALUES ('bacon burger', '0');
-INSERT INTO `burgers_db`.`burgers` (`burger_name`, `devoured`) VALUES ('cheese burger', '0');
-INSERT INTO `burgers_db`.`burgers` (`burger_name`, `devoured`) VALUES ('Wapper', '0');
+INSERT INTO `burgers` (`burger_name`, `devoured`) VALUES ('bacon burger', '0');
+INSERT INTO `burgers` (`burger_name`, `devoured`) VALUES ('cheese burger', '0');
+INSERT INTO `burgers` (`burger_name`, `devoured`) VALUES ('Wapper', '0');
