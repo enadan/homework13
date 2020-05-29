6a38178e183d1ec39dbbb3a39b6bde51a4a5f0c3
enandan <jl830630@gmail.com>
2020-05-28 19:44:24 -0700

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 
 var mysql = require('mysql');
+var connection;
 
-const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database : "burgers_db",
-    multipleStatements: true
-  });
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'password',
+        database : "burgers_db",
+        multipleStatements: true
+    });
+};
 
+connection.connect();
 
 module.exports = connection;
